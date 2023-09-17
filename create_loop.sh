5956377c679a0581ad0b826c63043232b086e87b
yui468 <me648ruadoresu@gmail.com>
2023-09-18 02:59:45 +0900

    新規追加

---
+++ create_loop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <file_prefix> <file_extension> <number_of_files>"
+  exit 1
+fi
+
+file_prefix="$1"      # ファイルのプレフィックス
+file_extension="$2"   # ファイルの拡張子
+num_files="$3"        # 生成するファイルの数
+
+# 引数が数値でない場合はエラーメッセージを表示して終了
+if ! [[ "$num_files" =~ ^[0-9]+$ ]]; then
+  echo "Error: Number of files must be a positive integer."
+  exit 1
+fi
+
+# ファイルを生成するループ
+for ((i=0; i<=num_files; i++)); do
+  filename="${file_prefix}_${i}${file_extension}"
+  touch "$filename"
+  echo "Created: $filename"
+done
+
+echo "JSON files created in the current directory"
